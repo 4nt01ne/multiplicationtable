0796a2781aace3b146aef522896329db0926f4b9
wilsant <Antoine.Wils@i8c.com>
2020-05-29 23:55:40 +0200

    add i18n (internationalisation)

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # multiplicationtable
-CLI to exercise multiplication to 10
+CLI to exercise multiplication to 10.
+Support different languages: en/GB, en/US, nl/BE, fr/BE, nl/NL, fr/FR.
 Written in Java 8
 Build: gradle assemble
 Execute:  java -jar build/libs/multiplicationtable.jar
+    This will execute the exercices with the default nl/BE language.
+Execute in a different language: java -jar build/libs/multiplicationtable.jar [language] [country]
+    example from english: java -jar build/libs/multiplicationtable.jar en GB
--- src/main/java/Mutiply.java
+++ src/main/java/Mutiply.java
@@ -4,15 +4,24 @@ import java.time.Instant;
 import java.util.Scanner;
 import java.util.Random;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 class Multiply implements AutoCloseable {
 	public static void main(String[] args) throws Exception
 	{
 		try (Multiply multiply = new Multiply();) {
+			if(args.length == 2) {
+				multiply.currentLocale = new Locale(String.valueOf(args[0]), String.valueOf(args[1]));
+				multiply.messages = ResourceBundle.getBundle("Messages", multiply.currentLocale);
+			}
 			multiply.run();
 		}
 	}
 
-	Scanner scanner = new Scanner(System.in);
+	private Scanner scanner = new Scanner(System.in);
+	private Locale currentLocale = new Locale("nl", "BE");
+        private ResourceBundle messages = ResourceBundle.getBundle("Messages", currentLocale);
 
 	public int readNextInt() {
 		while(scanner.hasNext()){
@@ -48,15 +57,15 @@ class Multiply implements AutoCloseable {
 	public void run() {
 		int goodAnswers = 0;
 		int answerCount = 0;
-		Screen screen = new Screen("Hoeveel hoefeningen zou je graag doen? ");
+		Screen screen = new Screen(messages.getString("how.much.exercises") + " ");
 		
 		screen.presentInitialInput();
 		int wantedExercises = readNextInt();
 		screen.append(wantedExercises).appendNewLine().print();
 
-		screen.append("Met intussen tijd? (j/n) ").printNoCarriageReturn();
+		screen.append(messages.getString("with.intermediate.time")).append(" ").printNoCarriageReturn();
 		char answer = readNextChar();
-		boolean withIntermediateTime = answer == 'j';
+		boolean withIntermediateTime = answer == messages.getString("answer.yes").charAt(0);
 		screen.append(answer).print();
 
 		Instant multiplyStart = Instant.now();
@@ -70,13 +79,13 @@ class Multiply implements AutoCloseable {
 			screen.append(currentExercise.getResult()).print();
 			if(currentExercise.isCorrect())	goodAnswers++;
 			if(withIntermediateTime) {
-				screen.append(" ").append(currentExercise.computeOutcome()).append(" in ").append(Duration.between(exerciseStart, Instant.now()).getSeconds()).append(" seconden").print();
+				screen.append(" ").append(currentExercise.computeOutcome()).append(" ").append(messages.getString("in.seconds.in")).append(" ").append(Duration.between(exerciseStart, Instant.now()).getSeconds()).append(" ").append(messages.getString("in.seconds.seconds")).print();
 			} else {
 				screen.append(" ").append(currentExercise.computeOutcome()).print();
 			}
 		}
 
-		screen.appendNewLine().append("totaal: ").append(goodAnswers).append("/").append(wantedExercises).append(" in ").append(Duration.between(multiplyStart, Instant.now()).getSeconds()).append(" seconden").print();
+		screen.appendNewLine().append(messages.getString("total")).append(": ").append(goodAnswers).append("/").append(wantedExercises).append(" ").append(messages.getString("in.seconds.in")).append(" ").append(Duration.between(multiplyStart, Instant.now()).getSeconds()).append(" ").append(messages.getString("in.seconds.seconds")).print();
 	}
 
 	public void close() throws Exception{
