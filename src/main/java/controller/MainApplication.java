1ec6622d88b56cb2a1abef5ce977a6b429047cf9
wilsant <Antoine.Wils@i8c.com>
2020-07-01 22:45:51 +0200

    switch to spring boot web + camel

---
+++ src/main/java/controller/MainApplication.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages =  {"controller", "web"})
+public class MainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MainApplication.class, args);
+  }
+}
---
+++ src/main/java/controller/ServletInitializer.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(MainApplication.class);
+  }
+}
